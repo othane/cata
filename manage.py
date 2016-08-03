0f316925f21720960e88f14eb85cc6c4b92c8eed
Oliver Thane <othane@gmail.com>
2016-08-03 19:03:10 +1200

    change django's port
    I am using 8000 for normal day to day stuff so this stops conflicts :)

--- manage.py
+++ manage.py
@@ -5,6 +5,11 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cata.settings")
 
-    from django.core.management import execute_from_command_line
+    # override default port
+    import django
+    django.setup()
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "8080"
 
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
